a87390cdda3eaff255488849af91f310cf8474b4
gabriel <pgabrieldeveloper@outlook.com>
2023-12-17 09:20:13 -0300

    criando use case uploader file useCase

--- src/main/java/com/pggm/s3/imageupload/useCases/interfaces/IUploaderUseCase.java
+++ src/main/java/com/pggm/s3/imageupload/useCases/interfaces/IUploaderUseCase.java
@@ -3,4 +3,5 @@ package com.pggm.s3.imageupload.useCases.interfaces;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface IUploaderUseCase extends UseCaseIN<MultipartFile> {
+
 }
